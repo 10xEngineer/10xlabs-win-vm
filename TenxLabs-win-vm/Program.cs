cce67fd70045d7f3708d60073bbff50ed1e40217
Radim Marek <radim@laststation.net>
2012-07-03 15:07:06 +0000

    Service skeleton

---
+++ TenxLabs-win-vm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+
+namespace TenxLabsService
+{
+    class Program : ServiceBase
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase.Run(new Program());
+        }
+
+        public Program()
+        {
+            this.ServiceName = "10xLabs Windows VM";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}
---
+++ TenxLabs-win-vm/TenxLabsServiceInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration.Install;
+using System.ComponentModel;
+
+namespace TenxLabsService
+{
+    [RunInstaller(true)]
+    public class TenxLabsServiceInstaller : Installer
+    {
+        public TenxLabsServiceInstaller()
+        {
+            var processInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            var serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+
+            processInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+
+            // service details
+            serviceInstaller.DisplayName = "10xLabs Windows VM";
+            serviceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Manual;
+
+            // Same as Program.ServiceName
+            serviceInstaller.ServiceName = "10xLabs Windows VM";
+
+            this.Installers.Add(processInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+
+    }
+}
