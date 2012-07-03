6be528bc09fa42ba726321d85fa9d13a33adbef5
Radim Marek <radim@laststation.net>
2012-07-03 21:02:00 +0000

    Microcloud API client, with resource notify support

---
+++ 10xlabs-win-vm/Microcloud.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using RestSharp;
+using System.Globalization;
+using System.Dynamic;
+using System.Data.Entity.Design.PluralizationServices;
+
+namespace TenxLabsService
+{
+    class Microcloud
+    {
+        private RestClient _client;
+        private PluralizationService ps;
+
+        public Microcloud()
+        {
+            // FIXME hardcoded endpoint
+            this._client = new RestClient("http://bunny.laststation.net:8080/");
+            this.ps = PluralizationService.CreateService(CultureInfo.GetCultureInfo("en-us"));
+        }
+
+        public void notify(string resource, string res_id, string action, Dictionary<string, object> descriptor)
+        {
+            var resources = ps.Pluralize(resource);
+
+            var request = new RestRequest(resources + "/{id}/notify", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddUrlSegment("id", res_id);
+
+            // prepare request body
+            dynamic flexible = new ExpandoObject();
+            var body = (IDictionary<string, object>) flexible;
+
+            body.Add("action", action);
+            body.Add(resource, descriptor);
+
+            request.AddBody(body);
+
+            var response = this._client.Execute(request);
+
+            var log = File.AppendText("c:\\10x.log");
+            log.WriteLine(DateTime.Now);
+            log.WriteLine(response.Content);
+            log.Close();
+
+        }
+    }
+}
--- 10xlabs-win-vm/Program.cs
+++ 10xlabs-win-vm/Program.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.ServiceProcess;
 
 namespace TenxLabsService
 {
     class Program : ServiceBase
     {
+        private Microcloud microcloud;
+
         static void Main(string[] args)
         {
             ServiceBase.Run(new Program());
@@ -16,16 +19,37 @@ namespace TenxLabsService
         public Program()
         {
             this.ServiceName = "10xLabs Windows VM";
+            this.microcloud = new Microcloud();
+
+            this.log("10xlabs created");
+
+
         }
 
         protected override void OnStart(string[] args)
         {
             base.OnStart(args);
+
+            this.log("onStart");
+
+            microcloud.notify("node", "sharp-vm", "confirm", null);
+
         }
 
         protected override void OnStop()
         {
             base.OnStop();
+
+            this.log("10xlab stopped");
         }
+
+        protected void log(string something)
+        {
+            var log = File.AppendText("c:\\10x.log");
+            log.WriteLine(DateTime.Now);
+            log.WriteLine(something);
+            log.Close();
+        }
+
     }
 }
