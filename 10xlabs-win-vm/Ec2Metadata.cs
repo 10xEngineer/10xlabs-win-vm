da5bc7cdaa6f49c84cdc6422065d6920607c0be5
Radim Marek <radim@laststation.net>
2012-07-04 11:13:55 +0000

    Added support custom user data

--- 10xlabs-win-vm/Ec2Metadata.cs
+++ 10xlabs-win-vm/Ec2Metadata.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using RestSharp;
+using RestSharp.Contrib;
 using System.Net;
 
 namespace TenxLabsService
@@ -10,25 +11,47 @@ namespace TenxLabsService
     class Ec2Metadata
     {
         private static string META_END = "http://169.254.169.254/";
-        private static string META_DATA = "latest/meta-data/{key}";
+        private const string META_DATA = "latest/meta-data/{key}";
+
+        public static Boolean onEC2()
+        {
+            var instanceId = getKey("instance-id");
+
+            if (instanceId == null) return false;
+
+            return true;
+        }
 
         public static string getKey(string key)
         {
-            string res = null;
+            var response = getClient().Execute(buildRequest(key));
 
-            var client = new RestClient(META_END);
-
-            var request = new RestRequest(META_DATA, Method.GET);
-            request.AddUrlSegment("key", key);
-
-            var response = client.Execute(request);
-
-            if (response.StatusCode != HttpStatusCode.OK)
-            {
-                return null;
-            }
+            if (response.StatusCode != HttpStatusCode.OK) return null;
 
             return response.Content;
+        }
+
+        public static string getCustomData()
+        {
+            var response = getClient().Execute<RestSharp.Serializers.JsonSerializer>(buildRequest("user-data", "/latest/{key}"));
+
+            if (response.StatusCode != HttpStatusCode.OK) return null;
+
+            return response.Content;
+        }
+
+
+        public static RestRequest buildRequest(string key, string path = META_DATA)
+        {
+            var request = new RestRequest(path, Method.GET);
+            request.AddUrlSegment("key", key);
+
+            return request;
+        }
+
+        public static RestClient getClient()
+        {
+            return new RestClient(META_END); ;
         }
 
     }
